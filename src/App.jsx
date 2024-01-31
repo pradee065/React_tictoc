a293923aed05cac991b671c0fe79eee073c9ee04
pradee065 <pradeepbharthu065@gmail.com>
2024-01-31 23:42:00 +0530

    Game Board changes - 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { useState } from "react";
 import Player from "./Components/Player.jsx";
 import GameBoard from "./Components/GameBoard.jsx";
 import Log from "./Components/Log.jsx";
+
+function deriveActivePlayer(gameTurns){
+  let currentActivePlayer = 'X';
+    if(gameTurns.length && gameTurns[0].player === 'X'){
+        currentActivePlayer = '0';
+    }
+    return currentActivePlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
-  const [activePlayer, setActivePlayer] = useState('X');
-
+  //const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex,colIndex){
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? '0' : 'X');
+    //setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? '0' : 'X');
+    deriveActivePlayer(gameTurns);
     setGameTurns((prevTurn) => {
-        let currentActivePlayer = 'X';
-        if(prevTurn.length && prevTurn[0].player === 'X'){
-          currentActivePlayer = '0';
-        }
+      const currentActivePlayer = deriveActivePlayer(prevTurn);
         const updatedTurn = [{square : {row: rowIndex, col: colIndex}, player : currentActivePlayer},...prevTurn];
         return updatedTurn;
     });
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -31,7 +31,7 @@ for (const turn of turns){
             <ol>
                 {row.map((playerSymbol, colIndex) => 
                 <li key={colIndex}>
-                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
                 </li>)}
             </ol>
             </li>)}
